bd7f9bfca21aab5d2e2a04975179ad155ea06ad0
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-11-23 16:17:25 +0100

    Create atom for todolist

--- src/recoil/atoms/todoListAtom.ts
+++ src/recoil/atoms/todoListAtom.ts
@@ -1,6 +1,9 @@
 import { atom } from "recoil";
 import { TodoItem } from "../../components/TodoItemView";
 
-export const todoListState = {}
+export const todoListState = atom<TodoItem[]>({
+  key: "TodoList",
+  default: [],
+});
 
-export const todoListFilterState = {}
+export const todoListFilterState = {};
